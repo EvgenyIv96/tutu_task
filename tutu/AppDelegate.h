293e6aa96e6f0f2a7b381c05e58dd43a55027b01
Evgeny <riunter@gmail.com>
2016-10-22 21:35:34 +0300

    First commit

---
+++ tutu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tutu
+//
+//  Created by Евгений Иванов on 22.10.16.
+//  Copyright © 2016 Евгений Иванов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tutu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tutu
+//
+//  Created by Евгений Иванов on 22.10.16.
+//  Copyright © 2016 Евгений Иванов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
